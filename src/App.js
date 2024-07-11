7263271c39855b2d623953c243dbf0cc319ab937
SusannaMj <141831885+SusannaMj@users.noreply.github.com>
2024-07-11 23:04:35 +1000

    Created basic folder structure and basic App.js structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages";
+import About from "./pages/about";
+import Dogbreeds from "./pages/dogbreeds";
+import Pups from "./pages/pups";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/dogbreeds" element={<Dogbreeds />} />
+        <Route path="/pups" element={<Pups />} />
+      </Routes>
+    </Router>
   );
 }
 
