10af8bcc932fe48060713fb80201664177bf914a
SusannaMj <141831885+SusannaMj@users.noreply.github.com>
2024-07-12 00:13:01 +1000

    Created NavbarElements component and imported to Navbar
    - imported NavLink from the react-router-dom library and renamed it as Link to navigate between different pages in application
    - imported the styled-components library to write CSS code within JavaScript to makes it easier to style the components

---
+++ src/components/Navbar/NavbarElements.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { NavLink as Link } from "react-router-dom";
+import styled from "styled-components";
+
+export const Nav = styled.nav`
+  background: #ffb3ff;
+  display: flex;
+  padding: 10px;
+`;
+
+export const NavLink = styled(Link)`
+  text-decoration: none;
+  display: flex;
+  padding-right: 20px;
+`;
+
+export const NavMenu = styled.div`
+  display: flex;
+  align-items: center;
+`;
