b0f435550a71472f158f5ef7a3532c43ed3d1902
SusannaMj <141831885+SusannaMj@users.noreply.github.com>
2024-07-12 23:14:29 +1000

    created homepage "get started "button
    - imported useNavigate
    - created link to pups-page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Home() {
+  const navigate = useNavigate();
+  function showPups() {
+    navigate("/pups");
+  }
   return (
     <div>
-      <h1>Adopt today!</h1>
+      <h1>Ready to meet your new best friend?</h1>
+      <button onClick={showPups}>Let's go!</button>
     </div>
   );
 }
