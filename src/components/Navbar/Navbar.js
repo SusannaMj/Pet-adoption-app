34e028a3d78208dd7dc187dba1b1c268650c872a
SusannaMj <141831885+SusannaMj@users.noreply.github.com>
2024-07-12 23:13:10 +1000

    added Homepage link to navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,6 +5,7 @@ export default function Navbar() {
   return (
     <Nav>
       <NavMenu>
+        <NavLink to="/">Home</NavLink>
         <NavLink to="/about">About</NavLink>
         <NavLink to="/dogbreeds">Dogbreeds</NavLink>
         <NavLink to="/pups">Meet the pups</NavLink>
