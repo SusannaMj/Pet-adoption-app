0db112547dc7f28514b2292888239462e6a5d4b4
SusannaMj <141831885+SusannaMj@users.noreply.github.com>
2024-07-14 19:19:39 +1000

    integrated Dogbreed api

--- src/pages/dogbreeds.js
+++ src/pages/dogbreeds.js
@@ -4,9 +4,28 @@ import axios from "axios";
 export default function Dogbreeds() {
   let [dogbreed, setDogbreed] = useState("");
 
+  function handleResponse(response) {
+    console.log(response);
+  }
+
   function handleSubmit(event) {
     event.preventDefault();
     alert(`You are searching for ${dogbreed}`);
+
+    //https://docs.apiverve.com/api/dogbreeds
+
+    let apiKey = "74a290c3-9db8-4d82-80a6-14cf81faf808";
+    let apiUrl = "https://api.apiverve.com/v1/dogbreeds?breed=beagle";
+
+    axios
+      .get(apiUrl, {
+        headers: {
+          "x-api-key": apiKey,
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      })
+      .then(handleResponse);
   }
 
   function updateDogbreed(event) {
