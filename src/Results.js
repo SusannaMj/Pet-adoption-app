b039e585d4793b9b63f912f91debfb69530fce44
SusannaMj <141831885+SusannaMj@users.noreply.github.com>
2024-07-16 22:19:25 +1000

    mapped through results (if existing) and display in new Types-component

--- src/Results.js
+++ src/Results.js
@@ -1,6 +1,22 @@
 import React from "react";
+import Types from "./Types";
 
 export default function Results(props) {
   console.log(props.results);
-  return <div>Hello</div>;
+  if (props.results) {
+    return (
+      <div>
+        <h2>{props.results[0].name}</h2>
+        {props.results.map(function (result, index) {
+          return (
+            <div key={index}>
+              <Types type={result} />
+            </div>
+          );
+        })}
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
---
+++ src/Types.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Types(props) {
+  return (
+    <div>
+      <h2>{props.type.name}</h2>
+      <h3>{props.type.life_span}</h3>
+      <img src={props.type.image.url} />
+    </div>
+  );
+}
