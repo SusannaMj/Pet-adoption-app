147654a0072fc0947bdcfcdad8e2e538f113f6c2
SusannaMj <141831885+SusannaMj@users.noreply.github.com>
2024-07-17 22:21:07 +1000

    set up Profile-component

---
+++ src/Profile.js
@@ -0,0 +1,4 @@
+import React from "react";
+export default function Profile() {
+  return <div className="profile-card">Hello</div>;
+}
--- src/pages/pups.js
+++ src/pages/pups.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Profile from "../Profile";
 
 export default function Pups() {
   return (
     <div>
       <h1>Meet the pups!</h1>
+      <Profile />
     </div>
   );
 }
