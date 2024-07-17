2ff0dd060983794cc75a28140445d902fca58b3d
SusannaMj <141831885+SusannaMj@users.noreply.github.com>
2024-07-18 00:36:11 +1000

    added basic styling hero homepage

--- src/Profile.js
+++ src/Profile.js
@@ -1,4 +1,11 @@
 import React from "react";
+
+//documentation: https://www.petrescue.com.au/api/docs
+//documentation API authentification: https://www.petrescue.com.au/api/docs/authentication
+//documentation: API response: https://www.petrescue.com.au/api/docs/listings_search
+
+//or : https://documenter.getpostman.com/view/17710041/UUy38kte
+
 export default function Profile() {
   return <div className="profile-card">Hello</div>;
 }
