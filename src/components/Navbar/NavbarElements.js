d0434d26d699ebeedb6eb2ac401ec68d1ca8d691
SusannaMj <141831885+SusannaMj@users.noreply.github.com>
2024-07-20 22:35:18 +1000

    styled Navigation bar

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -2,15 +2,17 @@ import { NavLink as Link } from "react-router-dom";
 import styled from "styled-components";
 
 export const Nav = styled.nav`
-  background: #ffb3ff;
   display: flex;
-  padding: 10px;
+  padding: 25px;
+  font-size: 15px;
+  font-weight: bold;
 `;
 
 export const NavLink = styled(Link)`
   text-decoration: none;
   display: flex;
   padding-right: 20px;
+  color: black;
 `;
 
 export const NavMenu = styled.div`
