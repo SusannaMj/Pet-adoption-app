3833823e2e6bc0d3619866e068240d935d911893
SusannaMj <141831885+SusannaMj@users.noreply.github.com>
2024-07-20 22:34:59 +1000

    styled homepage

--- src/pages/home.js
+++ src/pages/home.js
@@ -14,18 +14,23 @@ export default function Home() {
   return (
     <div>
       <header className="hero">
-        <h1>Ready to meet your new best friend?</h1>
-        <p>
-          Browse available dogs looking for a fur-ever home <br />
-          Your new best friend is waiting to meet you!
-        </p>
+        <div className="hero-textbox">
+          <h1>Ready to adopt?</h1>
+          <p>
+            Browse available dogs looking for a fur-ever home <br />
+            Your new best friend is waiting to meet you!
+          </p>
 
-        <button onClick={showPups}>Let's go!</button>
+          <button onClick={showPups}>Let's go!</button>
+        </div>
       </header>
-      <h2>
-        Curious what dogbreed might match with you? Try our dogbreed-match-maker
-      </h2>
-      <button onClick={navigateToDogbreeds}>Find my match</button>
+      <div className="main">
+        <h2>Curious what dogbreed might match with you?</h2>
+        <p>Try our dogbreed library to see if you will be a good match!</p>
+        <button onClick={navigateToDogbreeds} className="navigateToDogbreeds">
+          Find my match
+        </button>
+      </div>
     </div>
   );
 }
