225ffd4d771b51daad5ba902dfd98b63d3180738
SusannaMj <141831885+SusannaMj@users.noreply.github.com>
2024-07-22 22:58:08 +1000

    set up basic API/axios structure for adoptable pet search

--- src/pages/pups.js
+++ src/pages/pups.js
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import Profile from "../Profile";
+import axios from "axios";
 
 export default function Pups() {
+  let [age, setAge] = useState("");
+  let [breed, setBreed] = useState("");
+
+  function updateBreed(event) {
+    event.preventDefault();
+    setBreed(event.target.value);
+  }
+
+  function updateAge(event) {
+    event.preventDefault();
+    setAge(event.target.value);
+  }
+
+  function updateSearchInformation(event) {
+    let apiKey = "";
+    let apiUrl = "";
+
+    axios.get(apiUrl).then(handleApiResponse);
+  }
+
   return (
     <div>
       <h1>Meet the pups!</h1>
+      <form onSubmit={updateSearchInformation}>
+        <input type="number" onChange={updateAge} min="1" />
+        <input type="text" onChange={updateBreed} />
+        <input type="submit" value="search" />
+      </form>
       <Profile />
     </div>
   );
