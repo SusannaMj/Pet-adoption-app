250513bbd2bfab61ba5181a5122948217bc854b6
SusannaMj <141831885+SusannaMj@users.noreply.github.com>
2024-08-11 21:24:02 +1000

    moved dog introduction into separate  component

--- src/Profile.js
+++ src/Profile.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Introduction from "./Introduction";
 
 //documentation: https://dog.ceo/dog-api/documentation/breed
 
@@ -6,14 +7,17 @@ export default function Profile(props) {
   console.log(props.results.data);
   if (props.results) {
     return (
-      <div>
-        {props.results.map(function (result, index) {
-          return (
-            <div key={index}>
-              <img src={result} alt="profile-picture" className="dogImage" />
-            </div>
-          );
-        })}
+      <div className="profileCard">
+        <div className="profilePicture">
+          {props.results.map(function (result, index) {
+            return (
+              <div key={index}>
+                <img src={result} alt="profile-picture" className="dogImage" />{" "}
+                <Introduction />
+              </div>
+            );
+          })}
+        </div>
       </div>
     );
   } else {
