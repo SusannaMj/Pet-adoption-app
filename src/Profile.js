f0cb381fc474cbf2d0bfedcdd628a20ee05c308c
SusannaMj <141831885+SusannaMj@users.noreply.github.com>
2024-08-13 23:49:06 +1000

    added responsive image carousel to profile gallery
    - imported react carousel component
    - imported import "react-responsive-carousel/lib/styles/carousel.min.css" to error in displaying the carousel/slideshow

--- src/Profile.js
+++ src/Profile.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Introduction from "./Introduction";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 //documentation: https://dog.ceo/dog-api/documentation/breed
 
@@ -7,24 +9,18 @@ export default function Profile(props) {
   console.log(props.results.data);
   if (props.results) {
     return (
-      <div>
-        <div>
-          {props.results.map(function (result, index) {
-            return (
-              <div className="profileCard">
-                <div key={index} className="profilePicture">
-                  <img
-                    src={result}
-                    alt="profile-picture"
-                    className="dogImage"
-                  />{" "}
-                </div>{" "}
-                <Introduction imageUrl={result} />
-              </div>
-            );
-          })}
-        </div>
-      </div>
+      <Carousel useKeyboardArrows={true}>
+        {props.results.map(function (result, index) {
+          return (
+            <div className="profileCard">
+              <div key={index} className="profilePicture">
+                <img src={result} alt="profile-picture" className="dogImage" />{" "}
+              </div>{" "}
+              <Introduction imageUrl={result} />
+            </div>
+          );
+        })}
+      </Carousel>
     );
   } else {
     return null;
