710fdfda0992f0f952bd6a5762b3822862f8f8cc
SusannaMj <141831885+SusannaMj@users.noreply.github.com>
2024-08-14 23:57:06 +1000

    added and imported css file for the Profile component

--- src/Profile.js
+++ src/Profile.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Introduction from "./Introduction";
+import "./Profile.css";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
